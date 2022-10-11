0366a905d9bde1ef0ca1e5e255050fb3d02fe5a9
SJMoon <103993019+CentiHei@users.noreply.github.com>
2022-10-11 16:48:27 +0900

    비만도계산기 추가

--- 수학/계산기.java
+++ 수학/계산기.java
@@ -5,23 +5,29 @@ class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("첫 번째 수: ");
+        System.out.print("첫 번째 수: ");
         int a = scanner.nextInt();
-        System.out.println("+, -, *, /, % : ");
+        System.out.print("+, -, *, /, % : ");
         String b = scanner.next();
-        System.out.println("두 번째 수: ");
+        System.out.print("두 번째 수: ");
         int c = scanner.nextInt();
         System.out.println(solution.solution(a,b,c));
     }
 
     String solution(int a,String b,int c) {
-        String title = "hi";
-        if(b.equals("+")){}
-        else if(b.equals("-")){}
-        else if(b.equals("*")){}
-        else if(b.equals("/")){}
-        else if(b.equals("%")){}
-        else{}
-        return title;
+        String title = "###계산기### ";
+        int answer = 0;
+        String answer2 = "";
+        if(b.equals("+")){answer = a + c;}
+        else if(b.equals("-")){answer = a - c;}
+        else if(b.equals("*")){answer = a * c;}
+        else if(b.equals("/")){answer = a / c;}
+        else if(b.equals("%")){answer = a % c;}
+        else{System.out.println("그런거 없음.");}
+        return String.format("%s \n %d %s %d = %d", title,a,b,c,answer);
+    }
+
+    public char[] solution(int lan, int eng, int math) {
+        return null;
     }
 }
---
+++ 수학/성적표.java
@@ -0,0 +1,60 @@
+// """
+// 국어. 영어, 수학점수를 입력받아서 학점을 출력하는 프로그램을 완성하시오.
+// 각 과목 점수는 0 ~ 100 사이이다.
+// 평균에 따라 다음과 같이 학점이 결정된다
+// 90이상은 A학점
+// 80이상은 B학점
+// 70이상은 C학점
+// 60이상은 D학점
+// 50이상은 E학점
+// 그 이하는 F학점
+
+// 출력되는 결과는 다음과 같다.
+
+// ### 성적표 ###
+// ********************************
+// 이름 국어 영어 수학 총점 평균 학점
+// *******************************
+// 홍길동 90 90 92 272 90.6 A
+// ********************************
+// """
+
+package 수학;
+
+import java.util.Scanner;
+
+class 성적표 {
+    public static void main(String[] args) {
+        성적표 solution = new 성적표();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("국어: ");
+        int lan = scanner.nextInt();
+        System.out.print("영어: ");
+        int eng = scanner.nextInt();
+        System.out.print("수학: ");
+        int math = scanner.nextInt();
+        System.out.println(solution.solution(lan, eng, math));
+        
+    }
+
+    String solution(int lan, int eng, int math) {
+        String title = "###성적표###";
+        String name = "문성준";
+        int total = lan + eng + math;
+        float avg = total/3;
+        String grade = "";
+        if(avg >= 90){
+            grade = "A";
+        } else if(avg < 90 & avg >= 80){
+            grade = "B";
+        } else if(avg < 80 & avg >= 70){
+            grade = "C";
+        } else if(avg < 70 & avg >= 60){
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        return String.format("%s\n****************************************************************\n이름 국어 영어 수학 총점 평균 학점\n****************************************************************\n이름:%s 국어:%d 영어:%d 수학:%d 총점:%d 평균:%.1f 학점:%s\n****************************************************************", title,name,lan,eng,math,total,avg,grade);
+
+    }
+}
